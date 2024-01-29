d4fc6430570997dfb5ee2bc23d0bcc7007d00a8e
Faizan Naseer <111910355+faizannaseerr@users.noreply.github.com>
2024-01-29 16:04:33 -0500

    Update Upload.js

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -6,8 +6,8 @@ const Upload = () => {
       <div className="text-5xl font-bold pb-8">
         Summaries of the saved recordings
       </div>
-      <button className="hover:underline transition-all text-3xl font-semibold rounded-md">
-        
+      <button className="hover:underline transition-all text-5xl mt-20 font-semibold rounded-md">
+        No Recordings Yet.
       </button>
     </div>
   );
