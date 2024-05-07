a00e2eef309635a1374eb7924e287d630dbd6df0
Faizan Naseer <111910355+faizannaseerr@users.noreply.github.com>
2024-05-06 21:37:21 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 require("dotenv").config();
-// const fetch = require("node-fetch");
 
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const storage = multer.memoryStorage()
+const AWS = require("aws-sdk")
+const app = express();
+const upload = multer({ storage: storage })
 // const recordingRoutes = require("./routes/recordings");
 // const summarizationRoutes = require("./routes/summarization");
 
-const app = express();
-
 app.use(express.json()); // access req.body json passed on
+app.use(express.urlencoded({ extended: false }))
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
@@ -17,59 +20,82 @@ app.use((req, res, next) => {
 // middleware
 app.use(cors());
 
-app.post("/", async (req, res) => {
-  // console.log(req.files);
+const s3 = new AWS.S3({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY_ID,
+})
 
-  const audio = req.body;
-  console.log("Audio file:", audio);
+// helper for polling to receive transcription
+async function pollResult(resultUrl) {
+  let status;
+  do {
+    const response = await fetch(resultUrl, {
+      method: 'GET',
+      headers: {
+        'x-gladia-key': process.env.GLADIA_API,
+      },
+    });
+    const result = await response.json();
+    status = result.status;
+    console.log(status)
+    if (status === 'done') {
+      // Transcription is done, return the result
+      return result;
+    } else if (result.statusCode == 401 || result.statusCode == 404) {
+      // Transcription failed, handle error
+      throw new Error('Transcription failed - ', result.message);
+    }
+    // Wait for some time before making the next request
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 seconds
+  } while (status !== 'done');
+}
 
-  // console.log(audio);
-  // console.log(audioFile);
-  // const formData = new FormData();
-  // formData.append("audio", audio);
+// helper for uploading audio file to s3 bucket
+const uploadAudio = (filename, bucketname, file) => {
+  return new Promise((resolve, reject) => {
+    const params = {
+      Key: filename,
+      Bucket: bucketname,
+      Body: file,
+      ContentType: 'audio/mpeg',
+      ACL: 'public-read'
+    }
 
-  const options1 = {
-    method: "POST",
-    headers: {
-      "x-gladia-key": process.env.GLADIA_API,
-      "Content-Type": "application/json",
-    },
-    body: `{"audio_url": "${audio}"}`,
-  };
-
-  const audioURLupload = await fetch(
-    "https://api.gladia.io/v2/upload",
-    options1
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(options1);
-      console.log(response);
-      return response;
+    s3.upload(params, (err, data) => {
+      if (err) {
+        reject(err)
+      }
+      else {
+        console.log("done")
+        resolve(data)
+      }
     })
-    .catch((err) => console.error(err));
+  })
+}
 
-  // audioURL = audioURLupload.audio_url;
-
-  // const options = {
-  //   method: "POST",
-  //   headers: {
-  //     "x-gladia-key": process.env.GLADIA_API,
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: `{"audio_url": "${audio}"}`,
-  // };
-
-  // fetch("https://api.gladia.io/v2/transcription", options)
-  //   .then((response) => response.json())
-  //   .then((response) => {
-  //     console.log(response);
-  //     res.status(200).json(response);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //     res.status(400).json({ error: err });
-  //   });
+app.post("/summarize", upload.single("audio"), async (req, res) => {
+  try {
+    const link = await uploadAudio(req.file.originalname, 'audiointellectrecordings', req.file.buffer);
+    const options = {
+      method: "POST",
+      headers: {
+        "x-gladia-key": process.env.GLADIA_API,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ "audio_url": link.Location }),
+    };
+    const transcriptionResponse = await fetch("https://api.gladia.io/v2/transcription", options);
+    const transcriptionResult = await transcriptionResponse.json();
+    // Start polling the result URL
+    const result = await pollResult(transcriptionResult.result_url);
+    // Send the transcription result to the client
+    console.log(result)
+    const returnObject = { link: result.request_params.audio_url, name: result.file.filename, transcript: result.result.transcription.full_transcript }
+    res.status(200).json(returnObject);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error });
+  }
 });
 
 // app.use("", recordingRoutes);
