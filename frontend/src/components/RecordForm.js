292fe3a508f6facdcd86d9cf57899768737de28a
Faizan Naseer <111910355+faizannaseerr@users.noreply.github.com>
2024-06-23 02:35:27 -0400

    Update RecordForm.js

--- frontend/src/components/RecordForm.js
+++ frontend/src/components/RecordForm.js
@@ -46,16 +46,16 @@ const RecordForm = (props) => {
     };
 
     return (
-        <div className='border-4 border-[#000003] px-8 py-12 border-double shadow-md shadow-zinc-300 rounded-sm'>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.6 }} className='border-4 border-[#000003] px-8 py-12 border-double shadow-md shadow-zinc-300 rounded-sm'>
             <form className='flex flex-col max-w-[50rem]'>
-                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }} className="font-source font-medium mb-6">{summary}</motion.div>
+                <div className="font-source font-medium mb-6">{summary}</div>
                 <label className='font-medium mb-2'>Name</label>
                 <input onChange={titleChange} className='border-2 border-gray-400 rounded-sm focus:outline-gray-600 focus:rounded-sm active:outline-none py-1 px-2 mb-4'></input>
                 <label className='font-medium mb-2'>Categories</label>
                 <input onKeyDown={categoriesChange} className='border-2 border-gray-400 rounded-sm focus:outline-gray-600 active:outline-none py-1 px-2 mb-10'></input>
                 <div onClick={handleClick} className='cursor-pointer border-4 border-gray-400 border-double px-8 py-2 max-w-fit hover:border-black transition-all rounded-sm font-medium'>Save</div>
             </form>
-        </div>
+        </motion.div>
     )
 }
 
